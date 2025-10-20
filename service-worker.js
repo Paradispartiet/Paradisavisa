8b405d52b86ff67fbf4f5373995b4e03e1ef9c21
Paradispartiet <paradispartiet@gmail.com>
2025-10-20 13:12:41 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // service-worker.js
-const VERSION = "v2.1.15"; // endre dette hver gang du laster opp en ny versjon
+const VERSION = "v2.1.1.1"; // endre dette hver gang du laster opp en ny versjon
 
 self.addEventListener("install", event => {
   console.log("Service Worker installert – versjon:", VERSION);
